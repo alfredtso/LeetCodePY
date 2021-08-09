10dd3d2851afa270f3f2b3a5ac2828cc153eea70
Alfred Tso <alfredtso@gmail.com>
2021-08-09 12:37:20 +0800

    minmum height tree using BFS

--- minimum_height_trees.py
+++ minimum_height_trees.py
@@ -9,60 +9,63 @@ class Solution:
         >>> sol = Solution()
         >>> n = 6
         >>> edges = [[3,0],[3,1],[3,2],[3,4],[5,4]]
-        >>> sol.findMinHeightTrees(edges, n)
+        >>> sol.findMinHeightTrees(n, edges)
         [3, 4]
         >>> n = 1
         >>> edges = []
-        >>> sol.findMinHeightTrees(edges, n)
+        >>> sol.findMinHeightTrees(n, edges)
         [0]
         >>> n = 2
         >>> edges = [[0,1]]
-        >>> sol.findMinHeightTrees(edges, n)
+        >>> sol.findMinHeightTrees(n, edges)
         [0, 1]
 
         :param n:
         :param edges:
         :return:
         """
-        length = [-1]* (n-1)
 
+        # base case
+        if n <= 2:
+            return [i for i in range(n)]
 
+        # construct the adjacent list from edges, undirected graph
+        adj_list = [set() for i in range(n)]
+        for i, j in edges:
+            adj_list[i].add(j)
+            adj_list[j].add(i)
 
-        for x in range(n):
-            # for each x record the maximum number of edges to reach every other node
-            def bfs(edges, x):
-                # seen
-                seen = set()
+        # create a queue and enqueue all the leaf node (which is node with only one neighbor)
+        queue = collections.deque([])
+        for i in range(n):
+            if len(adj_list[i]) == 1:
+                queue.append(i)
 
-                # queue
-                q = collections.deque([x])
+        # BFS until we are left with 2 nodes
+        remaining_node = n
+        while remaining_node > 2:
+            remaining_node -= len(queue)
+            new_queue = collections.deque([])
+            # remove the leaf node and their associating edges
+            while queue:
+                leaf = queue.popleft()
 
-                # max edges
-                medges = -math.inf
+                # Get the neighbor since 1. we need to remove the edge, 2. the only possible next leaves are neighbors
+                neighbor = adj_list[leaf].pop()
+                adj_list[neighbor].remove(leaf)
 
-                count = 0
+                # check if neighbor becomes a leaf
+                if len(adj_list[neighbor]) == 1:
+                    new_queue.append(neighbor)
 
-                while q:
+            queue = new_queue
 
-                    node = q.popleft()
+        return list(queue)
 
-                    # find edges
-                    for e in edges:
-                        if node == e[0] and e[1] not in q and e[1] not in seen:
-                            q.append(e[1])
-                            count += 1
-                        elif node == e[1] and e[0] not in q and e[0] not in seen:
-                            q.append(e[0])
-                            count += 1
-
-                    seen.add(node)
-
-
-            bfs(edges, x)
-
-
-
-
-
-
-        # reture the nodes that have the minimum of the maximum number
+if __name__ == '__main__':
+    edges = [[3,0],[3,1],[3,2],[3,4],[5,4]]
+    n = 6
+    sol = Solution()
+    sol.findMinHeightTrees(n, edges)
+    import doctest
+    doctest.testmod()
